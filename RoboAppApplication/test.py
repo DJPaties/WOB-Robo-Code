7f53e4514c44599babed19010f31cdfc0f83c2bd
Pati$s <58045007+DJPaties@users.noreply.github.com>
2023-08-14 09:39:32 +0300

    Added a chatgpt API and a connection file

--- RoboAppApplication/test.py
+++ RoboAppApplication/test.py
@@ -1,89 +1,50 @@
-import speech_recognition as sr
-import os
+import requests
+import json
+import openai
 import time
-import glob
-import pygame
-from google.cloud import texttospeech 
 
-import os
-
-from google.cloud import speech
-import io
-import sounddevice as sd
-import numpy as np
-import soundfile as sf
+# Set your API key
+api_key = "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"
+openai.api_key = api_key
 
 
-# def tts(text):
-#     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'tts.json'
-#     client = texttospeech.TextToSpeechClient()
 
-#     # Detect the language of the input text
-#     language_code = detect(text)
+# Define the previous conversation (if any)
+previous_conversation = [
+    {'role': 'system', 'content': 'You are a helpful assistant.'}
+    # {'role': 'user', 'content': 'I want to eat'}
+    
+]
 
-#     ssml_text = '<speak>' + text + '</speak>'
-#     synthesis_input = texttospeech.SynthesisInput(ssml=ssml_text)
-#     voice = texttospeech.VoiceSelectionParams(
-#         language_code=language_code,
-#         ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
-#     )
-#     audio_config = texttospeech.AudioConfig(
-#         audio_encoding=texttospeech.AudioEncoding.MP3,
-#     )
-#     response = client.synthesize_speech(
-#         input=synthesis_input, voice=voice, audio_config=audio_config,
-#     )
-#     filename = 'audio' + str(int(time.time())) + '.mp3'
-#     with open(filename, 'wb') as out:
-#         out.write(response.audio_content)
+def send_response(msg):
+    start_time = time.time()
+    previous_conversation.append({'role': 'user', 'content': msg})
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=previous_conversation, 
+        temperature=0.7,
+        max_tokens=50,
+        stream=True
+    )
 
-#     pygame.mixer.init()
-#     pygame.mixer.music.load(filename)
-#     pygame.mixer.music.play()
-#     while pygame.mixer.music.get_busy():
-#         time.sleep(0.2)  # Wait a second before checking again
 
-#     # Clean up the generated audio files
-#     files = glob.glob('audio*.mp3')
-#     for f in files:
-#         try:
-#             os.remove(f)
-#         except OSError as e:
-#             print("Error: %s - %s." % (e.filename, e.strerror))
-      
-def speech_to_text():
-    start = time.time()
-    # Create a recognizer object
-    r = sr.Recognizer()
+    collected_contents = ""
 
-    # Open the microphone for capturing the speech
-    with sr.Microphone() as source:
-        print("Listening...")
+    # Iterate through the stream of events
+    for chunk in response:
+        delta = chunk['choices'][0]['delta']
+        if 'content' in delta:
+            content = delta['content']
+            collected_contents += content  # Append the content to the string
 
-        # Adjust the energy threshold for silence detection
-        r.energy_threshold = 4000
 
-        # Listen for speech and convert it to text
-        audio = r.listen(source)
+    print(collected_contents)
+    previous_conversation.append(
+        {'role': 'assistant', 'content': collected_contents}
+    )
+    print(time.time()-start_time)
+    print(previous_conversation)
 
-        try:
-            # Use the Google Web Speech API to recognize the speech in Arabic
-            text = r.recognize_google(audio, language='en-US')
-            print("You said:", text)
-            end = time.time()
-            elapsed = (end - start)*1000
-            print("Function time", elapsed)
-            # tts(text)
-            return text
-        
-        except sr.UnknownValueError:
-            x = "Speech recognition could not understand audio"
-            print(x)
-            # tts(x)
-            
-        except sr.RequestError as e:
-            x="Could not request results from Google Speech Recognition service; {0}".format(e)
-            print(x)
-            # tts(x)
-            
-speech_to_text()
+while True:
+    msg = input()
+    send_response(msg)
